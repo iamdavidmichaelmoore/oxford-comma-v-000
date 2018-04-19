d10b801226a71e5cc6206249143019a82e94236c
David Moore <hey@iamdavidmoore.com>
2018-04-19 01:49:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ def oxford_comma(array)
     array.join(",")
   elsif array_length == 2
     array.join(" and ")
-  elsif array_length = 3
-    array_commafied = array.join(", ")
-    "#{array_commafied}, and"
-  elsif array_length > 3
+  #elsif array_length = 3
+    #array_commafied = array.join(", ")
+    #{}"#{array_commafied}, and"
+  elsif array_length >= 3
     array_excluding_last_element = array.take(array[array_length - 1])
     array_commafied = array_excluding_last_element.join(", ")
     last_element = array.last
