5830c0acb8b1f97bf880835686565aa7c5ef57fd
David Moore <hey@iamdavidmoore.com>
2018-04-19 01:58:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
     array_commafied = array.join(", ")
     "#{array_commafied}, and"
   elsif array_length > 3
-    array_excluding_last_element = array.take(array_length-1)
+    puts array_excluding_last_element = array.take(array_length-1)
     array_commafied = array_excluding_last_element.join(", ")
     last_element = array.last
     "#{array_commafied}, and #{last_element}"
